8e82731bf0a3277196056b160e011399508d1364
Markus-Kivinen <markus.kivinen2@gmail.com>
2026-03-18 18:23:48 +0200

    fix globals

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,24 @@
-// import globals from 'globals';
-import js from '@eslint/js';
+import globals from "globals";
+import js from "@eslint/js";
 
 export default [
   js.configs.recommended,
   {
-    files: ['**/*.js'],
+    files: ["**/*.js"],
     languageOptions: {
-      sourceType: 'module',
-      // globals: { ...globals.node},
-      ecmaVersion: 'latest',
+      sourceType: "module",
+      globals: { ...globals.node },
+      ecmaVersion: "latest",
     },
     rules: {
-      eqeqeq: 'error',
-      'no-trailing-spaces': 'error',
-      'object-curly-spacing': ['error', 'always'],
-      'arrow-spacing': ['error', { before: true, after: true }],
-      'no-console': 'off',
+      eqeqeq: "error",
+      "no-trailing-spaces": "error",
+      "object-curly-spacing": ["error", "always"],
+      "arrow-spacing": ["error", { before: true, after: true }],
+      "no-console": "off",
     },
   },
   {
-    ignores: ['dist/**'],
+    ignores: ["dist/**"],
   },
 ];
