8e1bd0fbabffa950e78f73bab1aa256fa328f2e8
Markus-Kivinen <markus.kivinen2@gmail.com>
2026-03-21 20:31:19 +0200

    support ES modules on isDate

--- src/isDate.js
+++ src/isDate.js
@@ -1,9 +1,13 @@
 import getTag from './.internal/getTag.js'
 import isObjectLike from './isObjectLike.js'
 import nodeTypes from './.internal/nodeTypes.js'
+import { types as utilTypes } from 'node:util'
 
 /* Node.js helper references. */
-const nodeIsDate = nodeTypes && nodeTypes.isDate
+const nodeIsDate =
+  (nodeTypes && nodeTypes.isDate) ||
+  (utilTypes && utilTypes.isDate) ||
+  globalThis.util?.types?.isDate
 
 /**
  * Checks if `value` is classified as a `Date` object.
@@ -20,6 +24,7 @@ const nodeIsDate = nodeTypes && nodeTypes.isDate
  * isDate('Mon April 23 2012')
  * // => false
  */
+
 const isDate = nodeIsDate
   ? (value) => nodeIsDate(value)
   : (value) => isObjectLike(value) && getTag(value) === '[object Date]'
